1e7315eea210942424742e00225fc6f8b269a733
Krasimir Todorov <krasytod@gmail.com>
2016-06-03 13:16:31 +0000

    final I hope

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var routes = require('./app/routes/index.js');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var session = require('express-session');
+var formidable = require('express-formidable');
+var fs = require('fs');
+
 
 var app = express();
 require('dotenv').load();
@@ -15,6 +18,7 @@ mongoose.connect(process.env.MONGO_URI);
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/common', express.static(process.cwd() + '/app/common'));
+app.use(formidable.parse());
 
 app.use(session({
 	secret: 'secretClementine',
@@ -25,7 +29,31 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-routes(app, passport);
+//routes(app, passport);
+
+app.get('/', function(req, res){
+    fs.readFile('./index.html',"utf8", function (err, html) {
+        if (err) {
+            throw err; 
+        }   
+            res.writeHeader(200, {"Content-Type": "text/html"});  
+            res.write(html);  
+            res.end(); 
+            return;
+    });
+	
+});
+
+app.post('/api/fileanalyse/', function(req, res){
+  //console.log(req.body[0].size);
+  var returnObject = { fileSize: req.body[0].size};
+  var json = JSON.stringify(returnObject);
+  //console.log(json)//
+  res.writeHead(200, {"Content-Type": "application/json"});
+  res.end(json);
+});
+
+
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
